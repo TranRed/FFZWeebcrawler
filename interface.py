595b66e8be60257c98914aeba703a0231b395cf6
TranRed <kupfer.nils@gmail.com>
2018-12-10 23:05:38 +0100

    initial commit

---
+++ interface.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'interface.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(801, 112)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(10, 30, 781, 22))
+        self.lineEdit.setObjectName("lineEdit")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(360, 60, 81, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 10, 161, 16))
+        self.label.setObjectName("label")
+        self.label_status = QtWidgets.QLabel(self.centralwidget)
+        self.label_status.setGeometry(QtCore.QRect(170, 10, 161, 16))
+        self.label_status.setStyleSheet("color: rgb(255, 0, 0);")
+        self.label_status.setText("")
+        self.label_status.setObjectName("label_status")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "WeebsOut"))
+        self.pushButton.setText(_translate("MainWindow", "Run"))
+        self.label.setText(_translate("MainWindow", "FrankerFaceZ Emote-URL:"))
+
---
+++ main.py
@@ -0,0 +1,59 @@
+from PyQt5 import QtWidgets
+import interface
+import requests
+import re
+
+def create_naughtylist(ui):
+    baseurl = ui.lineEdit.text() + '?c_page='
+    ui.label_status.setText("running ...")
+    err404 = False
+    page = 0
+    channelTag = '<a href="/channel/'
+    error = "<title>404"
+
+    file = open("naughtylist.txt","w")
+
+    while err404 == False:
+        page += 1
+        url = baseurl + str(page)
+        r = requests.get(url)
+
+        source = str(r.content)
+
+        for match in re.finditer(error, source):
+            err404 = True
+
+        if err404 == False:
+            for match in re.finditer(channelTag, source):
+                notEnded = True
+                i = match.end()
+                channel = ''
+                while notEnded == True:
+                    if source[i] == '"':
+                        notEnded = False
+                    else:
+                        channel = channel + source[i]
+                        i += 1
+                channel = channel + '\n'
+                file.write(channel)
+
+    file.close()
+    ui.label_status.setText("finished!")
+
+
+def connect_button(ui):
+    ui.pushButton.clicked.connect(lambda: create_naughtylist(ui))
+
+class MainWindow(QtWidgets.QMainWindow, interface.Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+if __name__ == "__main__":
+    import sys
+    #create application object
+    app = QtWidgets.QApplication(sys.argv)
+    ui = MainWindow()
+    connect_button(ui)
+    ui.show()
+    sys.exit(app.exec_())
